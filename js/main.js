e7a40cc1dee278681ba072f13ff3deb402e522a8
Mar Caballero Moryno <mar.caballero@hotmail.es>
2024-02-28 11:24:18 +0100

    Add server

--- js/main.js
+++ js/main.js
@@ -2,33 +2,54 @@
 
 const tasksList = document.querySelector ('.js-task-list');
 
-const tasks = [
-    {
-        name: 'Recoger setas en el campo',
-        completed: true
-    },
-    { 
-        name: 'Comprar pilas',
-        completed: true
-    },
-    { 
-        name: 'Poner una lavadora de blancos',
-        completed: true
-    },
-    {
-      name: 'Aprender cómo se realizan las peticiones al servidor en JavaScript',
-      completed: false,
-    },
-  ];
+const GITHUB_USER = 'Mar-CM';
+const SERVER_URL = `https://dev.adalab.es/api/todo/${GITHUB_USER}`;
 
-  for (const task of tasks){
-    const taskInput = `<input id="${task.name}" type="checkbox">`;
+let tasks = [];
+
+fetch(SERVER_URL)
+  .then((response) => response.json())
+  .then ((data) => {
+    console.log(data)
+
+    tasks = data.results; // Estamos llamando al objeto al array del objeto que contiene la info que se llama results 
+
+    for (const task of tasks){
+      const taskInput = `<input id="${task.id}" type="checkbox">`;
+      tasksList.innerHTML += `
+      <li class="task">${taskInput}${task.name}</li>
+      `}
+    })
+
+  
+  
+
+// const tasks = [
+//     {
+//         name: 'Recoger setas en el campo',
+//         completed: true
+//     },
+//     { 
+//         name: 'Comprar pilas',
+//         completed: true
+//     },
+//     { 
+//         name: 'Poner una lavadora de blancos',
+//         completed: true
+//     },
+//     {
+//       name: 'Aprender cómo se realizan las peticiones al servidor en JavaScript',
+//       completed: false,
+//     },
+//   ];
+
+  // for (const task of tasks){
+  //   const taskInput = `<input id="${task.name}" type="checkbox">`;
     
-    tasksList.innerHTML += `
-    <li class="task">${taskInput}${task.name}</li>
-    `
-
-    if(task.completed === true){
-      checkbox.checked
-    }
-  }
+  //   tasksList.innerHTML += `
+  //   <li class="task">${taskInput}${task.name}</li>
+  //   `
+  //   // if(task.completed === true){
+  //   //   checkbox.checked
+  //   // }
+  // }
