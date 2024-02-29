6a342cb4ce55ea1f80c4e21db1fdc7a94dc6e9df
bearm12 <bearm12493@gmail.com>
2024-02-29 09:42:05 +0000

    change function

--- js/main.js
+++ js/main.js
@@ -7,6 +7,19 @@ const SERVER_URL = `https://dev.adalab.es/api/todo/${GITHUB_USER}`;
 
 let tasks = [];
 
+const renderTask = () => {
+  for (const task of tasks){
+    if(task.completed === true){
+    tasksList.innerHTML += `
+    <li class="crossed task"><input id="${task.id}" type="checkbox" checked>${task.name}</li>
+    `} else {
+      tasksList.innerHTML += `
+      <li class="task"><input id="${task.id}" type="checkbox">${task.name}</li>
+      `
+    }
+}
+}
+
 fetch(SERVER_URL)
   .then((response) => response.json())
   .then ((data) => {
@@ -14,14 +27,22 @@ fetch(SERVER_URL)
 
     tasks = data.results; // Estamos llamando al objeto al array del objeto que contiene la info que se llama results 
 
-    for (const task of tasks){
-      const taskInput = `<input id="${task.id}" type="checkbox">`;
-      tasksList.innerHTML += `
-      <li class="task">${taskInput}${task.name}</li>
-      `}
+    renderTask();
     })
 
-  
+
+
+    const handleClickCheckbox = (event) => {
+      const inputId = event.target.id
+      const taskIndex = tasks.findIndex((task) => {
+        return  task.name === inputId
+      })
+
+      tasks.results[taskIndex].completed = true;
+      console.log(tasks)
+    }
+tasksList.addEventListener('click', handleClickCheckbox)
+ 
   
 
 // const tasks = [
